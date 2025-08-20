19851fb2ff85b28e04427748fa710d14fac57608
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-08-20 06:40:33 +0400

    wip

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from decouple import config
 
 class Settings(object):
-    DATABASE_URL:str = config('DATABASE_URL','var/data/proteus.db')
+    DATABASE_URL:str = config('DATABASE_URL','sqlite:////var/data/proteus.db')
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
